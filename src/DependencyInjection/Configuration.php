a41630ea5d6b099f92fb59e37e5c72f733b02c5f
programarivm <info@programarivm.com>
2020-02-16 09:18:13 +0000

    Symfony 5.0 TreeBuilder constructor has been updated

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -9,14 +9,15 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('knpu_lorem_ipsum');
+        $treeBuilder = new TreeBuilder('knpu_lorem_ipsum');
 
-        $rootNode
-            ->children()
-                ->booleanNode('unicorns_are_real')->defaultTrue()->info('Whether or not you believe in unicorns')->end()
-                ->integerNode('min_sunshine')->defaultValue(3)->info('How much do you like sunshine?')->end()
-            ->end()
+        $treeBuilder
+            ->getRootNode()
+                ->children()
+                    ->booleanNode('unicorns_are_real')->defaultTrue()->info('Whether or not you believe in unicorns')->end()
+                    ->integerNode('min_sunshine')->defaultValue(3)->info('How much do you like sunshine?')->end()
+                    ->scalarNode('word_provider')->defaultNull()->end()
+                ->end()
         ;
 
         return $treeBuilder;
